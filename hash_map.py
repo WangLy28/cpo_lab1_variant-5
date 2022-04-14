44dd7ce771286e00e50ff9faf2ab579d93c67c1d
WangLy28 <wlysbox@126.com>
2022-04-14 13:14:02 +0800

    first commit (modify1)

--- hash_map.py
+++ hash_map.py
@@ -1,79 +1,79 @@
 class hash_map(object):
-	def __init__(self,list=[],factor = 1):
-		self.table = [None,None,None,None]
-		self.factor = factor
-		i = len(self.table)
-		j = len(list)
-		while j >= i :
-			self.table = self.table + [None,None,None,None]*self.factor
-			i = i + 4*self.factor
-		for l in list:
-			index = l % 4
-			if self.table[index]==None :
-				self.table[index] = l
-			else :
-				i = 0
-				while i != 10:
-					index = index + 4
-					if index > len(self.table):
-						self.table = self.table + [None,None,None,None]
-						self.table[index] = l
-						break
-					else:
-						if self.table[index]==None :
-							self.table[index] = l
-							break
-						else :
-							i +=1
-							continue
+    def __init__(self, list=[], factor=1):
+        self.table = [None, None, None, None]
+        self.factor = factor
+        i = len(self.table)
+        j = len(list)
+        while j >= i:
+            self.table = self.table + [None, None, None, None] * self.factor
+            i = i + 4 * self.factor
+        for l in list:
+            index = l % 4
+            if self.table[index] is None:
+                self.table[index] = l
+            else:
+                i = 0
+                while i != 10:
+                    index = index + 4
+                    if index > len(self.table):
+                        self.table = self.table + [None, None, None, None]
+                        self.table[index] = l
+                        break
+                    else:
+                        if self.table[index] is None:
+                            self.table[index] = l
+                            break
+                        else:
+                            i += 1
+                            continue
 
-	def capacity(self):
-		return len(self.table)
-	
-	def length(self):
-		i = 0
-		for v in self.table:
-			if v != None:
-				i+=1
-		return i
+    def capacity(self):
+        return len(self.table)
 
-	def add_value(self,value):
-		i = self.capacity()
-		j = self.length() + 1
-		while j >= i :
-				self.table = self.table + [None,None,None,None]*self.factor
-				i = i + 4*self.factor
-		index = value % 4
-		if self.table[index]==None :
-			self.table[index] = value
-		else :
-			i = 0
-			while i != 10:
-				index = index + 4
-				if index > len(self.table):
-					self.table = self.table + [None,None,None,None]
-					self.table[index] = value
-					break
-				else:
-					if self.table[index]==None :
-						self.table[index] = value
-						break
-					else :
-						i +=1
-						continue
-		return self
+    def length(self):
+        i = 0
+        for v in self.table:
+            if v is not None:
+                i += 1
+        return i
 
-	def reduce_value(self,value):
-		index = value % 4
-		while value in self.table :
-			self.table[self.table.index(value)] = None
-		return self
+    def add_value(self, value):
+        i = self.capacity()
+        j = self.length() + 1
+        while j >= i:
+            self.table = self.table + [None, None, None, None] * self.factor
+            i = i + 4 * self.factor
+        index = value % 4
+        if self.table[index] == None:
+            self.table[index] = value
+        else:
+            i = 0
+            while i != 10:
+                index = index + 4
+                if index > len(self.table):
+                    self.table = self.table + [None, None, None, None]
+                    self.table[index] = value
+                    break
+                else:
+                    if self.table[index] == None:
+                        self.table[index] = value
+                        break
+                    else:
+                        i += 1
+                        continue
+        return self
 
-	def find_value(self,value):
-		if value in self.table :
-			return self.table.index(value)
-		else :
-			print('notfound')
+    def reduce_value(self, value):
+        index = value % 4
+        while value in self.table:
+            self.table[self.table.index(value)] = None
+        return self
+
+    def find_value(self, value):
+        if value in self.table:
+            return self.table.index(value)
+        else:
+            print('notfound')
 # t1 = hash_map([0,1,2,3,4,5,9,9]).add_value(13).reduce_value(9).find_value(9)
 # for v in t1.table:
 # 	print(v)
